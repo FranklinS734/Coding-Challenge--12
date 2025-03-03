b09e4e479bac11dea98e4b4098534365cad8b2c9
FranklinS734 <martinc3@usf.edu>
2025-03-02 20:47:50 -0500

    Task 3 - Implemented Dynamic Inventory List

--- cc_12.js
+++ cc_12.js
@@ -28,3 +28,42 @@ metricCardsArray.forEach(card => {
     card.innerText += " - Updated"; // Append text to indicate update
     card.style.backgroundColor = "#AD00FA"; // Change background color to purple
 });
+
+// Task 3: Dynamic Inventory Management – Adding and Removing Items
+
+const inventoryList = document.getElementById("inventoryList");
+
+function addInventoryItem(product) { // Write a function that creates a new <li> element representing a product.
+    let newLi = document.createElement("li"); 
+    newLi.setAttribute("class","product-item");
+    newLi.setAttribute("id",product); 
+    newLi.setAttribute("onclick",`removeInventoryItem("${product}")`);
+    newLi.textContent = product; 
+    inventoryList.appendChild(newLi); 
+};
+
+let productForm = document.getElementById('productForm');
+let error = document.getElementById('error');
+
+// Test Case
+addInventoryItem("Click me"); 
+addInventoryItem("Add a product below"); 
+
+productForm.addEventListener('submit', (event) => { 
+    let productName = document.getElementById('productName').value; 
+    if (productName === '') { 
+        error.textContent = 'Product name is required'; 
+        event.preventDefault(); 
+    } else {
+        error.textContent = '';
+        addInventoryItem(productName); 
+        event.preventDefault(); 
+    }
+});
+
+// Writing a separate function that removes a specific <li> when it is clicked. Use removeChild within the inventory list to remove the clicked item.
+function removeInventoryItem(id) { 
+    let invItem = document.getElementById(id);
+    inventoryList.removeChild(invItem); 
+}
+
